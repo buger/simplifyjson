7a6e91794ad9ebaa128fdba1b545d8daebb91fdb
Leonid Bugaev <leonsbox@gmail.com>
2023-05-21 19:11:37 +0300

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -1,3 +1,36 @@
 export function onRequest(context) {
-  return new Response("Hello, world!")
+  if (context.request.method == "GET") {
+    return new Response(`{"error": "POST method supported"}`, {status: 405})
+  }
+  
+  let body = await context.request.json()
+  return new Response(JSON.serialize(removeEmptyValues(body), null, 4))
+}
+
+function removeEmptyValues(obj) {
+  if (Array.isArray(obj)) {
+    return obj.map((item) => removeEmptyValues(item));
+  }
+
+  if (typeof obj === 'object' && obj !== null) {
+    const newObj = {};
+
+    for (const key in obj) {
+      const value = removeEmptyValues(obj[key]);
+
+      if (!value) {
+        continue
+      }
+
+      if (typeof value === 'object' && Object.keys(value).length === 0) {
+        continue; // Skip empty objects
+      }
+
+      newObj[key] = value;
+    }
+
+    return newObj;
+  }
+
+  return obj;
 }
